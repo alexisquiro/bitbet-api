f7eb8eaccfde2debc18699e262185bf294037d4d
alexis <sixela.quiros@gmail.com>
2022-08-01 15:33:30 -0400

    creado sql de la base

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,3 +1,28 @@
+CREATE TABLE Usuario(
+    id_user SERIAL PRIMARY KEY,
+    nombre_usuario VARCHAR(30) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	pass VARCHAR(100) NOT NULL
+);
+CREATE TABLE Game(
+    id_game SERIAL PRIMARY KEY,
+    team1 VARCHAR(30) NOT NULL,
+	team2 VARCHAR(30) NOT NULL,
+	game_date date NOT NULL,
+	winner integer default NULL
+);
+CREATE TABLE TIcket_Bet(
+    id_ticket SERIAL PRIMARY KEY,
+	id_user INTEGER NOT NULL,
+    id_game integer NOT NULL,
+	team VARCHAR (30) NOT NULL,
+	email_pay VARCHAR (50)NOT NULL,
+	hash_pay VARCHAR (100) NOT NULL,
+	amount INTEGER  NOT NULL,
+	confirm INTEGER NOT NULL,
+    CONSTRAINT FK_IdUser FOREIGN KEY (id_user) REFERENCES Usuario(id_user),
+    CONSTRAINT FK_IdGame FOREIGN KEY (id_game) REFERENCES Game(id_Game)
 
+);
 
 
